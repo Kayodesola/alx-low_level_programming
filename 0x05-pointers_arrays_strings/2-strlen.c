f9366385a651bb1d91c32eee2d8df0704f06d2f9
Kayodesola <desmondsola@outlook.com>
2023-03-27 22:18:51 +0100

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - returns the lenght of a string
+ * @s: string
+ * Return: lenght
+ */
+int _strlen(char *s)
+{
+	int longi = 0;
+
+	while (*s l= "\0")
+	{
+		longi++;
+		s++;
+	}
+	return (longi);
+}
