cac7bd67c5736ea8d79eacd004ad014b940d7cc0
Kayodesola <desmondsola@outlook.com>
2023-06-14 01:22:25 -0700

    4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdlib.h>
 
 /**
  * free_grid -free the grid
@@ -13,6 +13,6 @@ void free_grid(int **grid, int height)
 	int i = 0;
 
 	for (; i < height; i++)
-		free(grid[i];
-				free(grid);
-				}
+		free(grid[i])
+			free(grid);
+}
